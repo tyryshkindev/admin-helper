7a147ef1196aea432f22379fb1d74f3040303ff8
valeriy <tyryskin94@gmail.com>
2024-04-22 20:09:58 +0300

    removed HorizontalHeader from AppHeader, rewritten to own header component

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,16 +2,10 @@
     <header class="max-h-[6.5vh] bg-black">
         <nav class="container mx-auto p-3 flex justify-between">
             <NuxtLink to="/">
-                <img 
-                src="../public/hassle__logo.svg" 
-                alt="Hassle Logotype"
-                >
+                <img src="../public/hassle__logo.svg" alt="Hassle Logotype">
             </NuxtLink>
-            <div v-if="mainStore.isUserAuthorized">
-                <UHorizontalNavigation 
-                    class="bg-gray-200 rounded-md" 
-                    :links="links" 
-                />
+            <div v-if="mainStore.isUserAuthorized" class="flex flex-wrap bg-gray-200 rounded-md p-1.5">
+                <HeaderNavLinks :links="links"/>
             </div>
             <slot v-else>
                 <div class="flex">
@@ -23,6 +17,7 @@
 </template>
 <script setup>
 const mainStore = useMainAdminStore()
+
 const links = [{
     label: 'Профиль',
     icon: 'i-heroicons-home',
---
+++ components/HeaderNavLinks.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-for="(link, index) in links" :key="index" :class="{ 'border-b-2 border-sky-600': isLinkActive(link) }">
+        <HeaderNavLink :link="link"/>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    links: {
+        type: Array,
+        required: true
+    }
+})
+const {links} = toRefs(props)
+const route = useRoute()
+function isLinkActive(link) {
+    return route.path === link.to
+}
+</script>
