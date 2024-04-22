4264b309b164494a8aa1b15864ac3601f4f38651
valeriy <tyryskin94@gmail.com>
2024-04-22 17:27:57 +0300

    user value added to store

--- stores/MainAdminStore.js
+++ stores/MainAdminStore.js
@@ -1,5 +1,12 @@
 export const useMainAdminStore = defineStore('mainAdminStore', () => {
-    const user = reactive({})
+    const user = reactive({
+        "id": "1723454",
+        "nickname": "Valeriy_Tyryshkin",
+        "password": "javascriptizer777",
+        "lvl": "77",
+        "adminLvl": "4",
+        "alist": []
+      },)
     function writeUser(newData) {
         Object.assign(user, newData)
     }
