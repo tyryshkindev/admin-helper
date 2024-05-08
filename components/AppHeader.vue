2ce8e758ba916824f97f5989921c6b18297b48de
valeriy <tyryskin94@gmail.com>
2024-05-08 17:05:46 +0300

    lvl limitation in navigation implemented

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -5,7 +5,7 @@
                 <img src="../public/hassle__logo.svg" alt="Hassle Logotype">
             </NuxtLink>
             <div v-if="mainStore.isUserAuthorized" class="flex flex-wrap bg-gray-200 rounded-md p-1.5">
-                <HeaderNavLinks :links="links"/>
+                <HeaderNavLinks :links="links.value || []"/>
             </div>
             <slot v-else>
                 <div class="flex">
@@ -17,19 +17,38 @@
 </template>
 <script setup>
 const mainStore = useMainAdminStore()
-
-const links = [{
-    label: 'Профиль',
-    icon: 'i-heroicons-home',
-    to: '/profile',
-    badge: `${mainStore.user.nickname}`
-}, {
-    label: 'Статистика',
-    icon: 'i-heroicons-chart-bar',
-    to: '/statistics'
-}, {
-    label: 'Поиск игроков',
-    icon: 'i-heroicons-magnifying-glass',
-    to: '/players'
-}] 
+const links = reactive([])
+const linkParts = reactive({
+    profile: {
+        label: 'Профиль',
+        icon: 'i-heroicons-home',
+        to: '/profile',
+        badge: `${mainStore.user.nickname}`
+    }, 
+    statistics: {
+        label: 'Статистика',
+        icon: 'i-heroicons-chart-bar',
+        to: '/statistics',
+        badge: `${mainStore.user.nickname}`
+    }, search: {
+        label: 'Поиск игроков',
+        icon: 'i-heroicons-magnifying-glass',
+        to: '/players'
+    }
+})
+switch (mainStore.user.adminLvl) {
+    case '1': 
+        links.value = [{...linkParts.statistics}]
+        break
+    case '2': 
+    case '3':
+    case '4':
+        links.value = [{...linkParts.statistics}, {...linkParts.search}]
+        break
+    case '5':
+    case '6':
+        links.value = [{...linkParts.profile},{...linkParts.statistics}]
+        break
+    
+}
 </script>
